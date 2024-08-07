b0be2da0fb0bd3ff77da543321882a8850e0a5c6
Marcos1327 <giovannygigi@gmail.com>
2024-08-06 23:08:02 -0300

    created screen login

--- App.js
+++ App.js
@@ -1,38 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons'; // Importando o ícone
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import Login from './src/Screens/login';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <ImageBackground 
-      source={{ uri: 'https://uploaddeimagens.com.br/images/004/821/527/full/eu_e_ela.jpeg?1722899308' }} 
-      style={styles.background}
-    >
-      <View style={styles.container}>
-        <Text style={styles.text}>EU TE AMO REBEKA FERNANDES DE JESUS</Text>
-        <FontAwesome name="heart" size={30} color="red" style={styles.icon} /> 
-        <StatusBar style="auto" />
-      </View>
-    </ImageBackground>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{headerShow: false}}
+                      name="Login" component={Login}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    resizeMode: 'cover', // or 'stretch'
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    marginTop: 150,
-    fontSize: 20,
-    color: 'white',
-  },
-  icon: {
-    marginBottom: 10, // Ajuste o valor conforme necessário
-  },
-});
