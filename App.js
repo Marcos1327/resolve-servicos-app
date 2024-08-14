98f92e38cb99aa1a7f123e7829aec643318cf87d
Marcos1327 <giovannygigi@gmail.com>
2024-08-14 04:25:41 -0300

    created a welcome page and login page

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { StatusBar } from 'react-native';
 
-
-import Login from './src/Screens/login';
-const Stack = createNativeStackNavigator();
+import { NavigationContainer } from '@react-navigation/native'
+import Routes from './src/routes';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{headerShow: false}}
-                      name="Login" component={Login}/>
-
-      </Stack.Navigator>
+      <StatusBar backgroundColor="#38A69D" barStyle="light-content"></StatusBar>
+      <Routes/>
     </NavigationContainer>
   );
 }
